14801b5cb2d48728137fbef6ee2b3479f8cdde05
Steve <dr.stevenpawley@gmail.com>
2017-12-12 20:57:44 -1000

    updated init

--- ml_geomodels/__init__.py
+++ ml_geomodels/__init__.py
@@ -1,3 +1,3 @@
 from .models import specificity_score, spatial_loocv, cross_val_scores, predict
 from .plotting import raster_plot, shiftedColorMap
-from .sampling import extractPoints, extractPolygons, extractPixels, sample_random, random_oversampling, random_undersampling, filter_points
+from .sampling import extractPoints, extractPolygons, extractPixels, sample_random, filter_points, get_random_point_in_polygon
