19f04931d44b215c040e8346bdefaa866db9f89f
Steve <dr.stevenpawley@gmail.com>
2018-01-01 22:53:23 -0700

    Package name change

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 setup(
-    name="Pyspatialml",
+    name="pyspatialml",
     version="0.1",
     author="Steven Pawley",
     author_email="steven.pawley@gmail.com",
     description=("Machine learning for GIS spatial modelling"),
     license="GNU",
-    keywords="grass gis",
-    url="https://github.com/stevenpawley/Pyspatialml",
-    packages=["Pyspatialml"]
+    keywords="GIS",
+    url="https://github.com/stevenpawley/pyspatialml",
+    packages=["pyspatialml"]
 )
