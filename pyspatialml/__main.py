48ac5b5a01f22d9194f915bc8c0e0efcd4da6dd9
Steven Pawley <dr.stevenpawley@gmail.com>
2019-02-23 20:53:14 -0700

    Added predfun and probfun

--- pyspatialml/__main.py
+++ pyspatialml/__main.py
@@ -1006,6 +1006,95 @@ class Raster(BaseRaster):
 
         return raster
 
+    @staticmethod
+    def _predfun(img, estimator):
+        """
+        Prediction function for classification or regression response
+        Args
+        ----
+        img : 3d numpy array of raster data
+        estimator : estimator object implementing 'fit'
+            The object to use to fit the data
+        Returns
+        -------
+        result_cla : 2d numpy array
+            Single band raster as a 2d numpy array containing the
+            classification or regression result
+        """
+
+        n_features, rows, cols = img.shape[0], img.shape[1], img.shape[2]
+
+        # reshape each image block matrix into a 2D matrix
+        # first reorder into rows,cols,bands(transpose)
+        # then resample into 2D array (rows=sample_n, cols=band_values)
+        n_samples = rows * cols
+        flat_pixels = img.transpose(1, 2, 0).reshape(
+            (n_samples, n_features))
+
+        # create mask for NaN values and replace with number
+        flat_pixels_mask = flat_pixels.mask.copy()
+
+        # prediction
+        result_cla = estimator.predict(flat_pixels)
+
+        # replace mask
+        result_cla = np.ma.masked_array(
+            data=result_cla, mask=flat_pixels_mask.any(axis=1))
+
+        # reshape the prediction from a 1D into 3D array [band, row, col]
+        result_cla = result_cla.reshape((1, rows, cols))
+
+        return result_cla
+
+    @staticmethod
+    def _probfun(img, estimator):
+        """
+        Class probabilities function
+        Args
+        ----
+        img : 3d numpy array of raster data [band, row, col]
+        estimator : estimator object implementing 'fit'
+            The object to use to fit the data
+        Returns
+        -------
+        result_proba : 3d numpy array
+            Multi band raster as a 3d numpy array containing the
+            probabilities associated with each class.
+            Array is in (class, row, col) order
+        """
+
+        n_features, rows, cols = img.shape[0], img.shape[1], img.shape[2]
+
+        mask2d = img.mask.any(axis=0)
+
+        # reshape each image block matrix into a 2D matrix
+        # first reorder into rows,cols,bands(transpose)
+        # then resample into 2D array (rows=sample_n, cols=band_values)
+        n_samples = rows * cols
+        flat_pixels = img.transpose(1, 2, 0).reshape(
+            (n_samples, n_features))
+
+        # predict probabilities
+        result_proba = estimator.predict_proba(flat_pixels)
+
+        # reshape class probabilities back to 3D image [iclass, rows, cols]
+        result_proba = result_proba.reshape(
+            (rows, cols, result_proba.shape[1]))
+
+        # reshape band into rasterio format [band, row, col]
+        result_proba = result_proba.transpose(2, 0, 1)
+
+        # repeat mask for n_bands
+        mask3d = np.repeat(a=mask2d[np.newaxis, :, :], repeats=result_proba.shape[0], axis=0)
+
+        # convert proba to masked array
+        result_proba = np.ma.masked_array(
+            result_proba,
+            mask=mask3d,
+            fill_value=np.nan)
+
+        return result_proba
+
     def append(self, other):
         """
         Setter method to add new RasterLayers to a Raster object
