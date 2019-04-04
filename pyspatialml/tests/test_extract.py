3f49ecfe81ad18b2d4c29a6a9d1c9eeb42365ba4
Steve <dr.stevenpawley@gmail.com>
2019-04-03 23:16:34 -0600

    Update simple tests

--- pyspatialml/tests/test_extract.py
+++ pyspatialml/tests/test_extract.py
@@ -10,12 +10,20 @@ os.chdir(os.path.join('.', 'pyspatialml', 'tests'))
 
 class TestExtract(TestCase):
 
+    band1 = 'lsat7_2000_10.tif'
+    band2 = 'lsat7_2000_20.tif'
+    band3 = 'lsat7_2000_30.tif'
+    band4 = 'lsat7_2000_40.tif'
+    band5 = 'lsat7_2000_50.tif'
+    band7 = 'lsat7_2000_70.tif'
+    predictors = [band1, band2, band3, band4, band5, band7]
+
     def test_extract_points(self):
 
         # extract training data from points
-        with rasterio.open('landsat.vrt') as src:
-            training_pt = geopandas.read_file('landsat96_points.shp')
-            X, y, xy = extract(dataset=src, response=training_pt, field='id')
+        stack = Raster(self.predictors)
+        training_pt = geopandas.read_file('landsat96_points.shp')
+        X, y, xy = stack.extract_vector(response=training_pt, field='id', return_array=True)
 
         # check shapes of extracted pixels
         self.assertTupleEqual(X.shape, (885, 6))
@@ -44,10 +52,9 @@ class TestExtract(TestCase):
     def test_extract_polygons(self):
 
         # extract training data from polygons
-        with rasterio.open('landsat.vrt') as src:
-
-            training_py = geopandas.read_file('landsat96_polygons.shp')
-            X, y, xy = extract(dataset=src, response=training_py, field='id')
+        stack = Raster(self.predictors)
+        training_py = geopandas.read_file('landsat96_polygons.shp')
+        X, y, xy = stack.extract_vector(response=training_py, field='id', return_array=True)
 
         # check shapes of extracted pixels
         self.assertTupleEqual(X.shape, (2264, 6))
@@ -57,16 +64,14 @@ class TestExtract(TestCase):
     def test_extract_lines(self):
 
         # extract training data from lines
-        with rasterio.open('landsat.vrt') as src:
-            training_py = geopandas.read_file('landsat96_polygons.shp')
-            training_lines = deepcopy(training_py)
-            training_lines['geometry'] = training_lines.geometry.boundary
-            X, y, xy = extract(dataset=src, response=training_lines, field='id')
+        training_py = geopandas.read_file('landsat96_polygons.shp')
+        training_lines = deepcopy(training_py)
+        training_lines['geometry'] = training_lines.geometry.boundary
+        stack = Raster(self.predictors)
+        X, y, xy = stack.extract_vector(response=training_lines, field='id', return_array=True)
 
     def test_extract_raster(self):
 
         # extract training data from labelled pixels
-        with rasterio.open('landsat.vrt') as src:
-            training_px = rasterio.open('landsat96_labelled_pixels.tif')
-            X, y, xy = extract(dataset=src, response=training_px)
-
+        training_px = rasterio.open('landsat96_labelled_pixels.tif')
+        X, y, xy = stack.extract_raster(response=training_px, value_name='id', return_array=True)
