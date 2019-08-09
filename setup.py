fc2fec0f9ddb5aeab31bdae63c282eb79d6273a0
Steven Pawley <dr.stevenpawley@gmail.com>
2019-08-09 09:35:51 -0600

    setup.py update

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
+    # package metadata
     name="pyspatialml",
-    version="0.1",
+    version="0.0.1",
     author="Steven Pawley",
     author_email="steven.pawley@gmail.com",
     description=("Machine learning for GIS spatial modelling"),
     license="GNU",
     keywords="GIS",
     url="https://github.com/stevenpawley/pyspatialml",
+
+    # files/directories to be installed with package
     packages=find_packages(),
+    package_data={
+        '': ['*.tif', '*.dbf', '*.prj', '*.shp', '*.shx'],
+    },
+    include_package_data=True,
+
+    # package dependencies
     install_requires=[
         'numpy>=1.10.0',
         'scipy>1.0.0',
@@ -21,7 +30,8 @@ setup(
         'matplotlib>=2.2.4',
         'sklearn'],
     python_requires='>=3.5',
+
+    # testing
     setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    include_package_data=True
+    tests_require=['pytest']
 )
