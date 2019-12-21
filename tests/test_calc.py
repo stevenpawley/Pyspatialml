b613d0da8c1cd97d2749a06baa347d5aa4b34d28
Steven Pawley <dr.stevenpawley@gmail.com>
2019-12-21 09:17:40 -0700

    Added test for calc method

---
+++ tests/test_calc.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+from pyspatialml import Raster
+import pyspatialml.datasets.nc as nc
+
+
+class TestCalc(TestCase):
+
+    predictors = [nc.band1, nc.band2, nc.band3, nc.band4, nc.band5, nc.band7]
+    stack = Raster(predictors)
+
+    def test_calc_with_2d_output(self):
+
+        def compute_outputs_2d_array(arr):
+            new_arr = arr[0, :, :] + arr[1, :, :]
+            return new_arr
+        
+        calculation = self.stack.calc(compute_outputs_2d_array, n_jobs=1)
+        
+        self.assertIsInstance(calculation, Raster)
+        self.assertEqual(calculation.count, 1)
+        self.assertEqual(calculation.read(masked=True).count(), 183418)
+
+    def test_calc_with_3d_output(self):
+
+        def compute_outputs_3d_array(arr):
+            arr[0, :, :] = arr[0, :, :] + arr[1, : :]
+            return arr
+        
+        calculation = self.stack.calc(compute_outputs_3d_array, n_jobs=1)
+
+        self.assertIsInstance(calculation, Raster)
+        self.assertEqual(calculation.count, 6)
+        self.assertEqual(calculation.read(masked=True).count(), 1052182)
+
+    def test_calc_with_multiprocessing(self):
+
+        def compute_outputs_2d_array(arr):
+            new_arr = arr[0, :, :] + arr[1, :, :]
+            return new_arr
+        
+        calculation = self.stack.calc(compute_outputs_2d_array, n_jobs=-1)
+        
+        self.assertIsInstance(calculation, Raster)
+        self.assertEqual(calculation.count, 1)
+        self.assertEqual(calculation.read(masked=True).count(), 183418)
