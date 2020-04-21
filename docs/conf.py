b7adfe091e0b56f90739e5a4153879d93cbfe2ac
Steven Pawley <dr.stevenpawley@gmail.com>
2020-04-21 11:16:18 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -49,11 +49,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
 # explicitly assign the master document
 master_doc = 'index'
 
