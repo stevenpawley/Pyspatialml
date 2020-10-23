18916cac633f719942b855a37b917dd43e1973b9
Steven Pawley <dr.stevenpawley@gmail.com>
2020-10-23 14:17:17 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'shapely>=1.6',
         'geopandas>=0.3',
         'matplotlib>=2.2.4',
-        'sklearn'],
+        'scikit-learn>=0.22'],
     python_requires='>=3.5',
 
     # testing
